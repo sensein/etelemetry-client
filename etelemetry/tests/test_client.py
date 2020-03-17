3eb0e3da85eda4fe8daee72d334f4e53aafa3399
Satrajit Ghosh <satra@mit.edu>
2020-03-17 18:35:28 -0400

    check different routes

--- etelemetry/tests/test_client.py
+++ etelemetry/tests/test_client.py
@@ -47,3 +47,7 @@ def test_check_available():
     repo = "github/hub"
     res = check_available_version(repo, "0.1.0")
     assert "version" in res
+    res = check_available_version(repo, res["version"])
+    assert "version" in res
+    res = check_available_version(repo, "1000.1.0")
+    assert "version" in res
