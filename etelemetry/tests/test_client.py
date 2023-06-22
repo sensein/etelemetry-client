760803ba1d5de2bdfcb1da0ab311f7788b700f72
Mathias Goncalves <mathiasg@stanford.edu>
2023-06-22 10:11:16 -0400

    Apply suggestions from code review
    Co-authored-by: Yaroslav Halchenko <debian@onerussian.com>

--- etelemetry/tests/test_client.py
+++ etelemetry/tests/test_client.py
@@ -4,11 +4,11 @@ from ..config import ET_ROOT
 from ..client import _etrequest, get_project, check_available_version
 
 
-def check_cxn():
+def check_cxn(scope="session"):
     import requests
 
     try:
-        requests.get("https://google.com")
+        requests.get("http://example.com")
         return True
     except Exception:
         return False
